042fdedee8c60bfa7194191d78ebc25a237c1ded
habibullo-dev <habibullochutboev@gmail.com>
2025-12-05 12:31:19 +0900

    feat: update Vercel and Vite configurations for dashboard routing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        dashboard: 'dashboard.html'
+      }
+    }
   }
 })
